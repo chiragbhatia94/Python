86fcda0273c6322094215b7dea46db113a9c8f4f
Chirag Bhatia <chiragbhatia94@gmail.com>
2015-12-04 18:49:42 +0530

    regex

---
+++ regex.py
@@ -0,0 +1,8 @@
+import re
+USER_RE = re.compile(r"^[a-zA-Z0-9_-]{3,20}$")
+PASSWORD_RE = re.compile(r"^.{3,20}$")
+EMAIL_RE = re.compile(r"^[\S]+@[\S]+\.[\S]+$")
+
+def isValid(q,p):
+    return q.match(p)
+
